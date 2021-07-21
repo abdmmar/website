20805da8a4e0ffa072534424633dbfec6048b4b7
abdmmar <opowae909@gmail.com>
2021-07-22 00:37:55 +0700

    Add hero section

--- pages/index.js
+++ pages/index.js
@@ -21,16 +21,58 @@ export default function Home({posts}) {
       </header>
 
       <main className={styles.main}>
-        <h1>Test</h1>
-        <ul>
-          {posts.map(({slug, frontmatter: meta}) => {
-            return (
-              <li key={slug}>
-                <Link href={`/posts/${slug}`}>{meta.title}</Link>
-              </li>
-            )
-          })}
-        </ul>
+        <div className={styles.hero}>
+          <div className={styles.hero_grid_one}>
+            <div></div>
+            <div className={styles.hero_content}>
+              <div className={styles.hero_desc}>
+                <h2 className={styles.hero_desc_title}>
+                  Hi! I&apos;m Ammar,
+                </h2>
+                <p>
+                  A computer science student and graphic design enthusiast who
+                  passionate to build something fun and useful.
+                </p>
+                <p>
+                  Currently remote working as Developer Intern at Telkom DDB,
+                  learning software development in general and trying to
+                  contribute to open source.
+                </p>
+              </div>
+            </div>
+            <div></div>
+          </div>
+          <div className={styles.hero_grid_two}>
+            <div></div>
+            <div className={styles.hero_social}>
+              <ul className={styles.hero_social_list}>
+                <li>
+                  <a href="#">Github</a>
+                </li>
+                <li>
+                  <a href="#">Figma</a>
+                </li>
+                <li>
+                  <a href="#">Email</a>
+                </li>
+              </ul>
+            </div>
+            <div></div>
+          </div>
+        </div>
+        <div className={styles.projects}></div>
+        <div className={styles.blog}>
+          <h1>Test</h1>
+          <ul>
+            {posts.map(({slug, frontmatter: meta}) => {
+              return (
+                <li key={slug}>
+                  <Link href={`/posts/${slug}`}>{meta.title}</Link>
+                </li>
+              )
+            })}
+          </ul>
+        </div>
       </main>
       <footer></footer>
     </div>
