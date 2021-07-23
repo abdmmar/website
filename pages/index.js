55e6986457b74a6759fc58535cdb341b8ec3ec01
abdmmar <opowae909@gmail.com>
2021-07-23 22:09:41 +0700

    Add projects section

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import Head from 'next/head'
 import Link from 'next/link'
+import Image from 'next/image'
 import {getAllPosts} from '@lib/mdx'
 import styles from './Index.module.scss'
 import {NavBar} from '@components/Layout'
@@ -21,7 +22,7 @@ export default function Home({posts}) {
       </header>
 
       <main className={styles.main}>
-        <div className={styles.hero}>
+        <div className={styles.hero_container}>
           <div className={styles.hero_grid_one}>
             <div></div>
             <div className={styles.hero_content}>
@@ -43,7 +44,7 @@ export default function Home({posts}) {
             <div></div>
             <div className={styles.hero_cta}>
               <a href="#projects" className={styles.hero_cta_link}>
-                <span>Learn more</span>
+                <h3 className={styles.hero_cta_link_text}>Learn more</h3>
                 <span>
                   <svg
                     width="30"
@@ -60,7 +61,51 @@ export default function Home({posts}) {
             <div></div>
           </div>
         </div>
-        <div className={styles.projects} id="projects"></div>
+        <div className={styles.projects_container} id="projects">
+          <div className={styles.projects}>
+            <div className={styles.projects_header}>
+              <h3 className={styles.projects_header_title}>Projects</h3>
+              <p className={styles.projects_header_desc}>
+                Here are some of my work for assignments, organizations,
+                events, and initiative.
+              </p>
+              <a href="/projects" className={styles.projects_header_link}>
+                See all projects
+                <svg
+                  width="24"
+                  height="30"
+                  viewBox="0 0 30 33"
+                  fill="currentColor"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <use href="#icon-arrow"></use>
+                </svg>
+              </a>
+            </div>
+            <div className={styles.projects_content}>
+              <div className={styles.projects_main_content}>
+                <div className={styles.projects_content_info}>
+                  <span className={styles.projects_content_tag}>Small</span>
+                  <span className={styles.projects_content_date}>
+                    23 Feb 2021
+                  </span>
+                </div>
+                <Image
+                  src="/Blue.jpg"
+                  alt="title"
+                  width="500px"
+                  height="300px"
+                  objectFit="cover"
+                />
+                <h4 className={styles.projects_content_title}>Title</h4>
+                <p className={styles.projects_content_desc}>Description</p>
+              </div>
+              <div className={styles.projects_aside_content}>
+                <div className={styles.projects_item}></div>
+              </div>
+            </div>
+          </div>
+        </div>
         <div className={styles.blog}>
           <h1>Test</h1>
           <ul>
