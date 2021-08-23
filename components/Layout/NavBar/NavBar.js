a184cf9b13afc363292fa00ac3baf2662f0bf441
abdmmar <opowae909@gmail.com>
2021-08-23 16:13:45 +0700

    Extract theme utility from navbar component

--- components/Layout/NavBar/NavBar.js
+++ components/Layout/NavBar/NavBar.js
@@ -9,8 +9,14 @@ import {
 } from '@reach/menu-button'
 import {positionRight} from '@reach/popover'
 import '@reach/menu-button/styles.css'
-import useWindowSize from '@hooks/useWindowSize'
 
+import {
+  redraw,
+  disableTransition,
+  enableTheme,
+  disableTheme,
+} from '@lib/theme'
+import useWindowSize from '@hooks/useWindowSize'
 import styles from './NavBar.module.scss'
 
 export default function NavBar() {
@@ -154,41 +160,4 @@ export default function NavBar() {
       </div>
     </nav>
   )
-}
-
-const enableTheme = (theme) => {
-  document.documentElement.setAttribute('data-theme', theme)
-  localStorage.setItem('theme', theme)
-}
-
-const disableTheme = () => {
-  document.documentElement.removeAttribute('data-theme')
-  localStorage.removeItem('theme')
-}
-
-/* 
-  Disable theme transitions
-  See: https://paco.sh/blog/disable-theme-transitions
-*/
-const disableTransition = () => {
-  const css = document.createElement('style')
-  css.appendChild(
-    document.createTextNode(
-      `* {
-        -webkit-transition: none !important;
-        -moz-transition: none !important;
-        -o-transition: none !important;
-        -ms-transition: none !important;
-        transition: none !important;
-      }`,
-    ),
-  )
-  document.head.appendChild(css)
-  return css
-}
-
-// Calling getComputedStyle forces the browser to redraw
-const redraw = (cssElem) => {
-  window.getComputedStyle(cssElem).opacity
-  document.head.removeChild(cssElem)
 }
