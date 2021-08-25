e0fa233a32c2e6f5d5ea85fc0cc0dd1293b7910e
abdmmar <opowae909@gmail.com>
2021-08-25 15:37:30 +0700

    Change to rehype

--- components/MDX/Paragraph/Paragraph.js
+++ components/MDX/Paragraph/Paragraph.js
@@ -1,7 +1,7 @@
 import {Image} from '@components/MDX'
 
 export default function Paragraph({children}) {
-  if (children?.type?.name === 'Image') {
+  if (children?.type?.name === 'Image1') {
     const {src, alt} = children.props
     return <Image src={src} alt={alt} />
   }
