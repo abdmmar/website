8595bdd190ebead6379ca29e5dbb4ca5dd239788
abdmmar <opowae909@gmail.com>
2021-08-27 14:12:04 +0700

    Update icon and logo

--- components/Head.js
+++ components/Head.js
@@ -33,7 +33,30 @@ export default function Head({
       <meta name="twitter:card" content="summary_large_image" />
       <meta name="twitter:creator" content="@abdmmar" />
 
-      <link rel="icon" href="/favicon.ico" />
+      {/* https://realfavicongenerator.net/ */}
+      {/* https://css-tricks.com/svg-favicons-and-all-the-fun-things-we-can-do-with-them/ */}
+      <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+      <link
+        rel="apple-touch-icon"
+        sizes="180x180"
+        href="/apple-touch-icon.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="32x32"
+        href="/favicon-32x32.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="16x16"
+        href="/favicon-16x16.png"
+      />
+      <link rel="manifest" href="/site.webmanifest" />
+      <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
+      <meta name="msapplication-TileColor" content="#2d89ef" />
+      <meta name="theme-color" content="#ffffff" />
     </NextHead>
   )
 }
--- components/Layout/NavBar/NavBar.js
+++ components/Layout/NavBar/NavBar.js
@@ -50,8 +50,20 @@ export default function NavBar() {
     <nav className={styles.navbar}>
       <div className={styles.navbar_menu}>
         <Link href="/">
-          <a className={styles.logo}>
-            <div className={styles.square}></div>
+          <a className={styles.logo_container}>
+            <svg
+              width="26"
+              height="23"
+              viewBox="0 0 26 23"
+              xmlns="http://www.w3.org/2000/svg"
+              className={styles.logo}
+            >
+              <path
+                fillRule="evenodd"
+                clipRule="evenodd"
+                d="M11.0974 0.491882L0.291046 22.1045H0.298437C1.68551 19.1103 4.11736 16.6988 7.12606 15.3379L12.791 4.00795L18.456 15.3379C16.7284 14.5565 14.8105 14.1215 12.7911 14.1215C7.25472 14.1215 2.48202 17.391 0.298454 22.1045H3.81517C5.71506 19.159 9.02531 17.2088 12.7911 17.2088C16.5568 17.2088 19.867 19.159 21.7669 22.1045L25.2836 22.1045H25.291L14.4847 0.491882H11.0974Z"
+              />
+            </svg>
           </a>
         </Link>
         <ul className={styles.menu}>
