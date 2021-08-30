bb272421084e2537ecb31cba1c100913ce4772d0
Abdullah Ammar <50788123+abdmmar@users.noreply.github.com>
2021-08-30 21:36:03 +0700

    Delete components/MDX directory

--- components/MDX/index.js
+++
@@ -1,6 +0,0 @@
-import Paragraph from './Paragraph/Paragraph'
-import Callout from './Callout/Callout'
-import Image from './Image/Image'
-import Link from './Link/Link'
-
-export {Callout, Image, Paragraph, Link}
