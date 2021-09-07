b6216dc11c9607353bdec1d3f04af46898ddbc37
abdmmar <opowae909@gmail.com>
2021-09-07 23:53:40 +0700

    style(detail projects): change information layout, add metadata, and change tag

--- pages/projects/[slug].js
+++ pages/projects/[slug].js
@@ -19,24 +19,28 @@ export default function DetailProjects({code, frontmatter: meta, slug}) {
     [code],
   )
 
-  const [category, ...tags] = meta.tags
-
   return (
     <Layout>
       <Head
         title={`${meta.title} • Abdullah Ammar • Developer`}
         path={`/projects/${slug}`}
-        description={meta.description != null ? meta.description : meta.title}
+        description={meta.description}
         type="article"
         image={meta.image}
+        image_alt={meta.image_alt}
+        keywords={meta.tags}
+        published_date={meta.published_date}
+        modified_date={meta.modified_date}
       />
 
       <main className={styles.main}>
         <div className={styles.hero_container}>
           <div className={styles.hero}>
             <div className={styles.hero_header}>
-              <span>{category}</span>
-              <span>{formatDate(meta.date, {dateStyle: 'long'})}</span>
+              <span>{meta.category}</span>
+              <span>
+                {formatDate(meta.published_date, {dateStyle: 'long'})}
+              </span>
             </div>
             <h2 className={styles.hero_title}>{meta.title}</h2>
             <Image
@@ -46,17 +50,41 @@ export default function DetailProjects({code, frontmatter: meta, slug}) {
               height="450px"
               width="900px"
             />
-            <div className={styles.hero_info}>
-              <span>
-                <a href={meta.repository} target="_blank" rel="noreferrer">
-                  Github
-                </a>{' '}
-                •{' '}
-                <a href={meta.website} target="_blank" rel="noreferrer">
-                  {meta.title}
-                </a>
-              </span>
-              <span>{tags.join(', ')}</span>
+            <div className={styles.hero_info_container}>
+              <dl className={styles.hero_info}>
+                <div>
+                  <dt>Repository</dt>
+                  <dd>
+                    <a
+                      href={meta.repository}
+                      target="_blank"
+                      rel="noreferrer"
+                    >
+                      Github
+                    </a>
+                  </dd>
+                </div>
+                <div>
+                  <dt>Demo</dt>
+                  <dd>
+                    <a href={meta.website} target="_blank" rel="noreferrer">
+                      {meta.title}
+                    </a>
+                  </dd>
+                </div>
+                <div>
+                  <dt>Status</dt>
+                  <dd>{meta.status}</dd>
+                </div>
+                <div>
+                  <dt>Author</dt>
+                  <dd>{meta.author.join(', ')}</dd>
+                </div>
+                <div>
+                  <dt>Tags</dt>
+                  <dd>{meta.tags.join(', ')}</dd>
+                </div>
+              </dl>
             </div>
           </div>
         </div>
@@ -64,9 +92,11 @@ export default function DetailProjects({code, frontmatter: meta, slug}) {
         <div className={styles.content_container}>
           <div className={styles.content}>
             <article className={styles.content_article}>
-              <p className={styles.content_article_deck}>
-                {meta.description}
-              </p>
+              {meta.subtitle != null || meta.subtitle != '' ? (
+                <p className={styles.content_subtitle}>{meta.subtitle}</p>
+              ) : (
+                ''
+              )}
               <Component
                 components={{
                   img: CustomImage,
